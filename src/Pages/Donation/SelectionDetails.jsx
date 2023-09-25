cc7d0088ba0c5c66bdbc273eb01298ae1834d10f
Masum-Reza <careerhunter4280@gmail.com>
2023-09-25 13:16:47 -0700

    dynamic route for details created and successfully showing details

--- src/Pages/Donation/SelectionDetails.jsx
+++ src/Pages/Donation/SelectionDetails.jsx
@@ -15,8 +15,15 @@ const SelectionDetails = () => {
 
     return (
         <div>
-            <div className="w-[50vw] mx-auto">
+            <div className="w-full mx-auto py-5 relative">
                 <img className="h-full w-full" src={picture} alt="" />
+                <div className="bg-[#0B0B0B80] w-full h-[25%] md:h-[20%] absolute bottom-5 flex items-center">
+                    <button className="text-white bg-[#FF444A] ml-6 px-3 py-1 rounded-sm hover:-translate-y-[0.10rem] active:translate-y-[0.10rem]">Donate ${price}</button>
+                </div>
+            </div>
+            <div className="space-y-3 pb-5">
+                <h1 className="font-bold text-xl md:text-2xl">{title}</h1>
+                <p className="text-justify">{description}</p>
             </div>
         </div>
     )
