230d2f1b28cfbfa462031ee25cdb97cf6462ab1b
Masum-Reza <careerhunter4280@gmail.com>
2023-09-25 03:32:25 -0700

    added functionality in see all btn

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -5,9 +5,16 @@ import SelectedDonations from "./SelectedDonations";
 
 const Donation = () => {
   const [allSelections, setAllSelections] = useState([]);
+  const [displayLength, setLength] = useState(4)
+  const [toggleBtn, setToggleBtn] = useState(true)
 
   const donationsData = useContext(donationsContext)
   const savedIds = getStoredData()
+
+  const handleSeeAll = () => {
+    setLength(allSelections.length)
+    setToggleBtn(!toggleBtn)
+  }
 
   useEffect(() => {
     let selectedDonations = []
@@ -19,11 +26,19 @@ const Donation = () => {
   }, [])
 
   return (
-    <div className="grid gap-5 grid-cols-1 md:grid-cols-2 py-5">
-      {
-        allSelections.map(item => <SelectedDonations key={item.id} item={item} />)
-      }
-    </div>
+    <>
+      <div className="grid gap-5 grid-cols-1 md:grid-cols-2 py-5">
+        {
+          allSelections.slice(0, displayLength).map(item => <SelectedDonations key={item.id} item={item} />)
+        }
+      </div>
+
+      <div className="text-center pb-5">
+        {toggleBtn &&
+          allSelections.length > 4 && <button className="bg-[#009444] text-white font-semibold px-3 py-1 rounded-md hover:-translate-y-[0.10rem] active:translate-y-[0.10rem]" onClick={handleSeeAll}>See All</button>
+        }
+      </div>
+    </>
   )
 }
 
