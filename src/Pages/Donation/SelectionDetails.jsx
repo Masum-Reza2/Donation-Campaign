b53dff90ec8b575ab4e5b0acaab7fd6fd4192b7e
Masum-Reza <careerhunter4280@gmail.com>
2023-09-25 13:38:17 -0700

    fixed save to LS functionality

--- src/Pages/Donation/SelectionDetails.jsx
+++ src/Pages/Donation/SelectionDetails.jsx
@@ -1,10 +1,11 @@
 import { useContext, useEffect, useState } from "react"
 import { donationsContext } from "../../MainLayOut/MainLayOut"
 import { useParams } from "react-router-dom"
+import { saveToLS } from '../../Utilities/LocalStorage';
 
 const SelectionDetails = () => {
     const [showingElement, setShowingElement] = useState({})
-    let { picture, price, title, description } = showingElement
+    let { picture, price, title, description, text_button_bg_color, id } = showingElement
     const detailElement = useContext(donationsContext)
     const { detailsId } = useParams()
 
@@ -18,7 +19,7 @@ const SelectionDetails = () => {
             <div className="w-full mx-auto py-5 relative">
                 <img className="h-full w-full" src={picture} alt="" />
                 <div className="bg-[#0B0B0B80] w-full h-[25%] md:h-[20%] absolute bottom-5 flex items-center">
-                    <button className="text-white bg-[#FF444A] ml-6 px-3 py-1 rounded-sm hover:-translate-y-[0.10rem] active:translate-y-[0.10rem]">Donate ${price}</button>
+                    <button onClick={() => saveToLS(id)} className="text-white ml-6 px-3 py-1 rounded-sm hover:-translate-y-[0.10rem] active:translate-y-[0.10rem]" style={{ backgroundColor: text_button_bg_color }}>Donate ${price}</button>
                 </div>
             </div>
             <div className="space-y-3 pb-5">
--- src/components/Rights/Right.jsx
+++ src/components/Rights/Right.jsx
@@ -1,20 +1,22 @@
 import PropTypes from 'prop-types';
-import { saveToLS } from '../../Utilities/LocalStorage';
+import { Link } from 'react-router-dom';
 
 const Right = ({ right }) => {
     let { id, picture, category, title, card_bg_color, text_button_bg_color, category_bg_color } = right;
 
 
     return (
-        <div onClick={() => saveToLS(id)} className={`space-y-3 border pb-3 rounded-md transition-all duration-100 hover:-translate-y-1 cursor-pointer active:scale-105`} style={{ backgroundColor: card_bg_color }}>
-            <div>
-                <img src={picture} alt="" />
+        <Link to={`/${id}`}>
+            <div className={`space-y-3 border pb-3 rounded-md transition-all duration-100 hover:-translate-y-1 cursor-pointer active:scale-105`} style={{ backgroundColor: card_bg_color }}>
+                <div>
+                    <img src={picture} alt="" />
+                </div>
+                <div className='space-y-1 font-bold pl-3' style={{ color: text_button_bg_color }}>
+                    <p className={`py-1 px-3 w-fit rounded-md text-sm`} style={{ backgroundColor: category_bg_color }}>{category}</p>
+                    <p>{title}</p>
+                </div>
             </div>
-            <div className='space-y-1 font-bold pl-3' style={{ color: text_button_bg_color }}>
-                <p className={`py-1 px-3 w-fit rounded-md text-sm`} style={{ backgroundColor: category_bg_color }}>{category}</p>
-                <p>{title}</p>
-            </div>
-        </div>
+        </Link>
     )
 }
 
