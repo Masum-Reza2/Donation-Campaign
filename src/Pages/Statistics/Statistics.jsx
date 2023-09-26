e17f9938c53709e573ac171ef515358861c2fff9
Masum-Reza <careerhunter4280@gmail.com>
2023-09-25 19:34:47 -0700

    explored and worket with chart and brooom! done

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,7 +1,59 @@
+import { useContext } from 'react';
+import { PieChart, Pie, Cell, ResponsiveContainer, Legend, Tooltip } from 'recharts';
+import { donationsContext } from '../../MainLayOut/MainLayOut';
+import { getStoredData } from '../../Utilities/LocalStorage';
+
 const Statistics = () => {
+
+  const totalDonation = useContext(donationsContext)
+  const myDonation = getStoredData()
+  // console.log(totalDonation, myDonation)
+
+  const data = [
+    { name: 'Total Donation', value: totalDonation.length },
+    { name: 'My Donation', value: myDonation.length },
+
+  ];
+
+  const COLORS = ['#FF444A', '#00C49F'];
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
+
+
   return (
-    <div>
-      this is statistics page
+    <div className="h-[80vh] flex items-center justify-center">
+      <ResponsiveContainer width="100%" height="100%">
+        <PieChart width={400} height={400}>
+          <Pie
+            data={data}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={80}
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+          <Legend />
+          <Tooltip />
+        </PieChart>
+      </ResponsiveContainer>
     </div>
   )
 }
