a669f6aec630f0301a1e66fd5ca02ee14b423e60
Masum-Reza <careerhunter4280@gmail.com>
2023-09-25 17:20:47 -0700

    added validation in empty search

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,9 @@ import Rights from "../../components/Rights/Rights"
 import { useContext, useState } from "react"
 import { donationsContext } from "../../MainLayOut/MainLayOut"
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const Home = () => {
   const donations = useContext(donationsContext)
 
@@ -14,12 +17,36 @@ const Home = () => {
   }
   const handleBtn = () => {
     setTogDep(!toggleDep)
+
+    !searchText && toast.info('Write something first!', {
+      position: "top-right",
+      autoClose: 2500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    })
   }
 
   return (
     <div>
       <Banner handleChange={handleChange} handleBtn={handleBtn} />
       <Rights donations={donations} searchText={searchText} toggleDep={toggleDep} />
+
+      <ToastContainer
+        position="top-right"
+        autoClose={2500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
     </div>
   )
 }
