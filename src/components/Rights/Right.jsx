3d1ea7e4094c3b649401c8f79b62366009c4a716
Masum-Reza <careerhunter4280@gmail.com>
2023-09-25 20:27:47 -0700

    updated the repeated picyires oj json

--- src/components/Rights/Right.jsx
+++ src/components/Rights/Right.jsx
@@ -9,7 +9,7 @@ const Right = ({ right }) => {
         <Link to={`/${id}`}>
             <div className={`space-y-3 border pb-3 rounded-md transition-all duration-100 hover:-translate-y-1 cursor-pointer active:scale-105`} style={{ backgroundColor: card_bg_color }}>
                 <div>
-                    <img src={picture} alt="" />
+                    <img className='h-full md:h-[30vh] w-full' src={picture} alt="" />
                 </div>
                 <div className='space-y-1 font-bold pl-3' style={{ color: text_button_bg_color }}>
                     <p className={`py-1 px-3 w-fit rounded-md text-sm`} style={{ backgroundColor: category_bg_color }}>{category}</p>
